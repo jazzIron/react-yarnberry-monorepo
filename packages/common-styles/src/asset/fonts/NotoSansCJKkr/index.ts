b8bfe6706b211f318836e1b8d08baa35971c6dd5
jazz.Iron <57489807+LeeJaeChul@users.noreply.github.com>
2022-06-21 20:39:36 +0900

    [feat] 폰트수정

--- packages/common-styles/src/asset/fonts/NotoSansCJKkr/index.ts
+++ packages/common-styles/src/asset/fonts/NotoSansCJKkr/index.ts
@@ -1,11 +1,4 @@
-// import NotoSansCJKkrRegular from './NanumBarunNotoSansCJKkrRegular.otf';
-// import NotoSansCJKkrBold from './NanumBarunNotoSansCJKkrBold.otf';
-// import NotoSansCJKkrLight from './NanumBarunNotoSansCJKkrLight.otf';
-// import NotoSansCJKkrThin from './NanumBarunNotoSansCJKkrThin.otf';
+import NotoSansCJKkrBold from '../NotoSansCJKkr/NotoSansCJKkr-Bold.woff2';
+import NotoSansCJKkrRegular from '../NotoSansCJKkr/NotoSansCJKkr-Regular.woff2';
 
-import NotoSansCJKkrBold from '../NotoSansCJKkr/NotoSansCJKkr-Bold.otf';
-import NotoSansCJKkrRegular from '../NotoSansCJKkr/NotoSansCJKkr-Regular.otf';
-import NotoSansCJKkrLight from '../NotoSansCJKkr/NotoSansCJKkr-Light.otf';
-import NotoSansCJKkrThin from '../NotoSansCJKkr/NotoSansCJKkr-Thin.otf';
-
-export { NotoSansCJKkrRegular, NotoSansCJKkrBold, NotoSansCJKkrLight, NotoSansCJKkrThin };
+export { NotoSansCJKkrRegular, NotoSansCJKkrBold };
